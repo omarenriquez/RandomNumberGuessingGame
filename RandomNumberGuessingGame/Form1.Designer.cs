5806b4d6366e424e3d870e07298582bc59bc2789
Omar Enriquez <tgoe912@gmail.com>
2016-10-26 08:38:42 -0700

    Add project files.

---
+++ RandomNumberGuessingGame/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEnterGuess = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStartOver = new System.Windows.Forms.Button();
+            this.txtAttemptNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(12, 226);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(12, 134);
+            this.txtAnswer.Multiline = true;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(260, 70);
+            this.txtAnswer.TabIndex = 1;
+            this.txtAnswer.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Answer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Guess";
+            // 
+            // txtEnterGuess
+            // 
+            this.txtEnterGuess.Location = new System.Drawing.Point(83, 87);
+            this.txtEnterGuess.Name = "txtEnterGuess";
+            this.txtEnterGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtEnterGuess.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(197, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStartOver
+            // 
+            this.btnStartOver.Location = new System.Drawing.Point(108, 226);
+            this.btnStartOver.Name = "btnStartOver";
+            this.btnStartOver.Size = new System.Drawing.Size(75, 23);
+            this.btnStartOver.TabIndex = 5;
+            this.btnStartOver.Text = "Start Over";
+            this.btnStartOver.UseVisualStyleBackColor = true;
+            this.btnStartOver.Click += new System.EventHandler(this.btnStartOver_Click);
+            // 
+            // txtAttemptNumber
+            // 
+            this.txtAttemptNumber.Location = new System.Drawing.Point(232, 53);
+            this.txtAttemptNumber.Name = "txtAttemptNumber";
+            this.txtAttemptNumber.ReadOnly = true;
+            this.txtAttemptNumber.Size = new System.Drawing.Size(35, 20);
+            this.txtAttemptNumber.TabIndex = 6;
+            this.txtAttemptNumber.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Attempt Number";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGuess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAttemptNumber);
+            this.Controls.Add(this.btnStartOver);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtEnterGuess);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEnterGuess;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStartOver;
+        private System.Windows.Forms.TextBox txtAttemptNumber;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RandomNumberGuessingGame/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random generator = new Random();
+            number = generator.Next(1, 100);
+
+            MessageBox.Show("Try to guess a number between 1 and 100");
+        }
+
+        int number;
+        int numberoftries = 1;
+       
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int guess = Convert.ToInt32(txtEnterGuess.Text);
+            
+            if (guess > number)
+            {
+                if (number % 2 == 0)
+                {
+                    txtAnswer.Text = ("Guess was too high, try again! the number is an even number!");
+                }
+                else if (number % 2 == 1)
+                {
+                    txtAnswer.Text = ("Guess was too high, try again! the number is an odd number!");
+                }
+            }
+
+            if (guess < number)
+            {
+                if (number % 2 == 0)
+                {
+                    txtAnswer.Text = ("Guess was too low, try again! the number is an even number!");
+                }
+                else if (number % 2 == 1)
+                {
+                    txtAnswer.Text = ("Guess was too low, try again! the number is an odd number!");
+                }
+            }
+
+            if (guess == number)
+            {
+                txtAnswer.Text = ("Awesome, guess was right!");
+
+                MessageBox.Show("It took you " + Convert.ToString(numberoftries) + " tries to guess the right number!");
+                btnStartOver.Focus();
+            }
+            ++numberoftries;
+            txtAttemptNumber.Text = numberoftries.ToString();
+            txtEnterGuess.Text = "";
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnStartOver_Click(object sender, EventArgs e)
+        {
+            txtAnswer.Text = "";
+            txtEnterGuess.Text = "";
+            txtAttemptNumber.Text = "";
+            numberoftries = 1;
+
+            txtEnterGuess.Focus();
+
+            Random generator = new Random();
+            number = generator.Next(1, 100);
+
+            MessageBox.Show("Try to guess a number between 1 and 100");
+
+        }
+    }
+}
