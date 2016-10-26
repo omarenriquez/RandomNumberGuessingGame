bad3f13f6e592fda3225abe5f471b93763280938
Omar Enriquez <tgoe912@gmail.com>
2016-10-26 08:43:42 -0700

    Changed the layout of the form

--- RandomNumberGuessingGame/Form1.Designer.cs
+++ RandomNumberGuessingGame/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.btnGuess = new System.Windows.Forms.Button();
             this.txtAnswer = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtEnterGuess = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
@@ -41,7 +40,7 @@
             // 
             // btnGuess
             // 
-            this.btnGuess.Location = new System.Drawing.Point(12, 226);
+            this.btnGuess.Location = new System.Drawing.Point(12, 209);
             this.btnGuess.Name = "btnGuess";
             this.btnGuess.Size = new System.Drawing.Size(75, 23);
             this.btnGuess.TabIndex = 0;
@@ -55,23 +54,14 @@
             this.txtAnswer.Multiline = true;
             this.txtAnswer.Name = "txtAnswer";
             this.txtAnswer.ReadOnly = true;
-            this.txtAnswer.Size = new System.Drawing.Size(260, 70);
+            this.txtAnswer.Size = new System.Drawing.Size(260, 59);
             this.txtAnswer.TabIndex = 1;
             this.txtAnswer.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Answer";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Location = new System.Drawing.Point(12, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 3;
@@ -79,15 +69,15 @@
             // 
             // txtEnterGuess
             // 
-            this.txtEnterGuess.Location = new System.Drawing.Point(83, 87);
+            this.txtEnterGuess.Location = new System.Drawing.Point(12, 93);
             this.txtEnterGuess.Name = "txtEnterGuess";
-            this.txtEnterGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtEnterGuess.Size = new System.Drawing.Size(65, 20);
             this.txtEnterGuess.TabIndex = 0;
             // 
             // btnExit
             // 
             this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnExit.Location = new System.Drawing.Point(197, 226);
+            this.btnExit.Location = new System.Drawing.Point(197, 209);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 4;
@@ -97,7 +87,7 @@
             // 
             // btnStartOver
             // 
-            this.btnStartOver.Location = new System.Drawing.Point(108, 226);
+            this.btnStartOver.Location = new System.Drawing.Point(107, 209);
             this.btnStartOver.Name = "btnStartOver";
             this.btnStartOver.Size = new System.Drawing.Size(75, 23);
             this.btnStartOver.TabIndex = 5;
@@ -107,7 +97,7 @@
             // 
             // txtAttemptNumber
             // 
-            this.txtAttemptNumber.Location = new System.Drawing.Point(232, 53);
+            this.txtAttemptNumber.Location = new System.Drawing.Point(121, 93);
             this.txtAttemptNumber.Name = "txtAttemptNumber";
             this.txtAttemptNumber.ReadOnly = true;
             this.txtAttemptNumber.Size = new System.Drawing.Size(35, 20);
@@ -117,7 +107,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(143, 60);
+            this.label3.Location = new System.Drawing.Point(99, 77);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 7;
@@ -136,7 +126,6 @@
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.txtEnterGuess);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAnswer);
             this.Controls.Add(this.btnGuess);
             this.Name = "Form1";
@@ -151,7 +140,6 @@
 
         private System.Windows.Forms.Button btnGuess;
         private System.Windows.Forms.TextBox txtAnswer;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtEnterGuess;
         private System.Windows.Forms.Button btnExit;
